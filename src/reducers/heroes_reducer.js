18152f7125d40b7e6c5a41a66a53b8e1f0a002e1
anniesnoopymd <anniesnoopymd@gmail.com>
2017-12-29 23:23:23 +0800

    create hero_reducer

---
+++ src/reducers/heroes_reducer.js
@@ -0,0 +1,13 @@
+import { ADD_CHARACTER } from '../actions';
+
+function heros(state = [], action){
+    switch(action.type) {
+      case ADD_CHARACTER:
+        let heros = [...state, createCharacter(action.id)];
+        return heros;
+      default:
+        return state;
+    }
+}
+
+export default heros;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -12,15 +12,15 @@ import { ADD_CHARACTER } from '../actions';
 //     }
 // }
 
-function heros(state = [], action){
-    switch(action.type) {
-      case ADD_CHARACTER:
-        let heros = [...state, createCharacter(action.id)];
-        return heros;
-      default:
-        return state;
-    }
-}
+// function heros(state = [], action){
+//     switch(action.type) {
+//       case ADD_CHARACTER:
+//         let heros = [...state, createCharacter(action.id)];
+//         return heros;
+//       default:
+//         return state;
+//     }
+// }
 
 function createCharacter(id) {
    let character = characters_json.find(c => c.id === id);
