d5c05bb59f7953ff1c334d21566960ab8a4f26d4
anniesnoopymd <anniesnoopymd@gmail.com>
2017-12-29 13:58:14 +0800

    create second stores

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,35 @@
+import { combineReducers } from 'redux';
 import characters_json from '../data/characters.json';
+import { ADD_CHARACTER } from '../actions';
 
 function characters(state = characters_json, action){
     switch(action.type){
+        case ADD_CHARACTER:
+            let characters = state.filter(item => item.id !== action.id );
+            return characters;
         default:
           return state;
     }
 }
 
-export default characters;
+function heros(state = [], action){
+    switch(action.type) {
+      case ADD_CHARACTER:
+        let heros = [...state, createCharacter(action.id)];
+        return heros;
+      default:
+        return state;
+    }
+}
+
+function createCharacter(id) {
+   let character = characters_json.find(c => c.id === id);
+   return character;
+}
+
+const rootReducer = combineReducers({
+    characters,
+    heros
+})
+
+export default rootReducer;
