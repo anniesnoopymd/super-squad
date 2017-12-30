1498ba735a02eb612d1464077c26a33891da6950
anniesnoopymd <anniesnoopymd@gmail.com>
2017-12-30 09:01:49 +0800

    mapping state ti props

--- src/component/CharacterList.js
+++ src/component/CharacterList.js
@@ -1,2 +1,20 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+
+
+class CharacterList extends Component{
+  render(){
+    return(
+      <div>
+        <h2>Characters</h2>
+      </div>
+    )
+  }
+}
+
+function mapStateToProps(state){
+  console.log('state', state);
+  return {};
+}
+
+export default connect(mapStateToProps, null)(CharacterList);
