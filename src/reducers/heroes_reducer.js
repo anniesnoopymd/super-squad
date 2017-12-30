0a6900ed38b2cf61806b307d31eca2b93d51ef94
anniesnoopymd <anniesnoopymd@gmail.com>
2017-12-30 08:11:55 +0800

    put import createCharacter into heros_reducer.js

--- src/reducers/heroes_reducer.js
+++ src/reducers/heroes_reducer.js
@@ -11,9 +11,4 @@ function heros(state = [], action){
     }
 }
 
-function createCharacter(id) {
-   let character = characters_json.find(c => c.id === id);
-   return character;
-}
-
 export default heros;
