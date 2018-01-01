b1e1822a48b8243102b17b978c97b0994c312a29
anniesnoopymd <anniesnoopymd@gmail.com>
2018-01-01 18:35:56 +0800

    create herolist component

--- src/component/CharacterList.js
+++ src/component/CharacterList.js
@@ -11,12 +11,15 @@ class CharacterList extends Component{
     return(
       <div>
         <h4>Characters</h4>
-        <ul>
+        <ul className="list-group">
           {
             this.props.characters.map(character => {
               return(
-                <li key={character.id}>
-                  <div>{character.name}</div>
+                <li key={character.id} className="list-group-item">
+                  <div className="list-item">{character.name}</div>
+                  <div className="list-item right-button" onClick={() => this.props.addCharacterById(character.id)}>
+                  +
+                  </div>
                 </li>
               )
             })
@@ -33,8 +36,8 @@ function mapStateToProps(state){
   };
 }
 
-function mapDispatchToProps(dispatch){
-  return bindActionCreators({ addCharacterById }, dispatch);
-}
+// function mapDispatchToProps(dispatch){
+//   return bindActionCreators({ addCharacterById }, dispatch);
+// }
 
-export default connect(mapStateToProps, mapDispatchToProps)(CharacterList);
+export default connect(mapStateToProps, {addCharacterById})(CharacterList);
